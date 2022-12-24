3aea9b7d903a7a8dc0c8565b703cbbf5da51046b
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-12-24 19:21:41 +0300

    Product details update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ const Home = ({products, bannerData}) => (
   </div>
 
   <div className="products-container">
-    {products?.map((product) => <Product key = {product._id} product={product} />)}
+    {products?.map((product) => <Product key =
+     {product._id} product={product} />)}
   </div>
 
   <FooterBanner footerBanner={bannerData && bannerData[0]} />
