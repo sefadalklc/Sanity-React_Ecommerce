90003775b14e1b0cc00bae2385953da5131ae838
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-12-24 17:47:50 +0300

    Navbar , footer , products update

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link';
 
-const Products = () => {
+import { urlFor } from '../lib/client';
+const Products = ( { product: {image , name , slug , price} }) => {
   return (
-    <div>Products</div>
+    <div>
+      <Link href={'/product/${slug.current}'}>
+        <div className="product-card">
+          <img src={urlFor(image && image[0])} 
+           width={250}
+           height={250}
+           className="product-image"
+           />
+           <p className="product-name">{name}</p>
+           <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,19 @@ import {client} from "../lib/client"
 import {Product, FooterBanner, HeroBanner, Footer} from "../components"
 
 const Home = ({products, bannerData}) => (
-    <div>
-    <HeroBanner HeroBanner={bannerData.length & bannerData[0]} />
-     
-    <div className='products-heading'>
-      <h2>Best Selling Products</h2>
-    <p>Speakers of many variations</p>
-    
-    </div>
+  <div>
+  <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+  <div className="products-heading">
+    <h2>Best Seller Products</h2>
+    <p>speaker There are many variations passages</p>
+  </div>
 
-    <div className='products-container'>
-      {products?.map ( (product) => product.name )}
-    </div>
-    
+  <div className="products-container">
+    {products?.map((product) => <Product key = {product._id} product={product} />)}
+  </div>
 
-
-    <FooterBanner />
-    </div>
+  <FooterBanner footerBanner={bannerData && bannerData[0]} />
+</div>
   )
 
 
