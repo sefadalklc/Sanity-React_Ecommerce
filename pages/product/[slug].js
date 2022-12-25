f2d2f1f7939b20f5cc3b651770e4ca5cdaa57ece
sefadalklc <sefadalk11@gmail.com>
2022-12-25 12:46:52 +0300

    qty is active after now cart logic

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,8 +8,11 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const {decQty,incQty, qty} = useStateContext();
+  const {decQty,incQty, qty, onAdd} = useStateContext();
 
+
+
+  
   const handleBuyNow = () => {
     onAdd(product, qty);
 
