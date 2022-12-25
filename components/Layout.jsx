158aa909b672ec609d5222e1551dc6c6fb3d816e
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-12-26 00:31:34 +0300

    Last updates

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>OSO Store</title>
+        <title>QTX Store</title>
       </Head>
       <header>
         <Navbar />
